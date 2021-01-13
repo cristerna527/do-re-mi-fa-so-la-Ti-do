fed638219af5660aaf11f217bb5bdac254ec7807
cristerna527 <77368131+cristerna527@users.noreply.github.com>
2021-01-12 18:16:51 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showLeds(`
+    # . # . #
+    . # # # .
+    . . # . .
+    . # # # .
+    # . # . #
+    `)
+music.playMelody("C5 B A G F E D C ", 120)
+soundExpression.twinkle.playUntilDone()
+basic.forever(function () {
+	
+})
